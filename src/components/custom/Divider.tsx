f23bd616908947bc1a97c297905572455d9ef708
Luks245 <florekl03c@lo5.bielsko.pl>
2023-09-01 01:14:49 +0200

    modified timezone offset

--- src/components/custom/Divider.tsx
+++ src/components/custom/Divider.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 const Divider = ({ date, margin }: { date: Date; margin: string }) => {
+  const timezoneOffset = new Date(date).getTimezoneOffset();
   return (
     <span className={`${margin} w-full text-center text-sm`}>
       {new Date(date).toDateString()}{" "}
-      {new Date(date).getHours().toString().padStart(2, "0")}:
-      {new Date(date).getMinutes().toString().padStart(2, "0")}
+      {(new Date(date).getHours() + timezoneOffset / 60 > 24
+        ? new Date(date).getHours() + timezoneOffset / 60 - 24
+        : new Date(date).getHours() + timezoneOffset / 60 < 0
+        ? new Date(date).getHours() + timezoneOffset / 60 + 24
+        : new Date(date).getHours() + timezoneOffset / 60
+      )
+        .toString()
+        .padStart(2, "0")}
+      :{new Date(date).getMinutes().toString().padStart(2, "0")}
     </span>
   );
 };
